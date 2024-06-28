cc2aa22f1efb03ce44eca45bd7d8c8c81a7cb1fd
amanbrikkin007 <144882962+amanbrikkin007@users.noreply.github.com>
2024-06-28 12:01:24 +0530

    - Add Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello There</h1>
-      </header>
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import '../App.css'
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
